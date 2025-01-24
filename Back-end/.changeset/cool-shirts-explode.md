ce0d354cc7bf968f200785bce269ba9223a0df65
Zoe Lecaille <zoe.lecaille@swan.io>
2025-01-24 10:25:23 +0100

    chore: create read me

---
+++ Back-end/.changeset/cool-shirts-explode.md
@@ -0,0 +1,5 @@
+---
+"back-end": patch
+---
+
+feat(swa-30): create a read me
--- Front-end/src/presentation/components/Header.tsx
+++ Front-end/src/presentation/components/Header.tsx
@@ -1,6 +1,6 @@
 import * as HoverCard from "@radix-ui/react-hover-card";
-import {AvatarIcon} from "@radix-ui/react-icons";
-import {useFetchLibrary} from "../hooks/FetchLibrary.tsx";
+import { AvatarIcon } from "@radix-ui/react-icons";
+import { useFetchLibrary } from "../hooks/FetchLibrary.tsx";
 
 const Header = () => {
   const { loading, error } = useFetchLibrary();
@@ -9,12 +9,12 @@ const Header = () => {
     <HoverCard.Root openDelay={0}>
       <nav className="bg-amber-950 text-white py-4 px-6 flex items-center justify-between shadow-md">
         <div>
-          <a href="/">
+          <a href="/swap-a-book/Front-end/public">
             <h1 className="text-3xl font-bold">Swap a Book</h1>
           </a>
         </div>
         <div className="flex items-center space-x-4">
-            <AvatarIcon className="w-7 h-7"/>
+          <AvatarIcon className="w-7 h-7" />
           <a href="/src/pages/LoginPage">
             <button
               className="ml-2 inline-block b-4 bg-white px-6 py-2 font-semibold rounded-3xl text-amber-950 shadow shadow-amber-950 hover:bg-amber-900 hover:text-white hover:font-semibold hover:border-amber-950"
@@ -24,7 +24,6 @@ const Header = () => {
             </button>
             {error && <p className="mt-4 text-red-500">{error}</p>}
           </a>
-
         </div>
       </nav>
     </HoverCard.Root>
